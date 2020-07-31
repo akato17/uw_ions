93db8475c669dc2eccefaf12f5d24c80b8cf3f73
akato17 <63743637+akato17@users.noreply.github.com>
2020-07-31 16:23:36 -0700

    Add files via upload

---
+++ mdconst.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 07:55:50 2020
+
+@author: Alex K.
+
+Introduce all simulation related constants for import
+"""
+from scipy.constants import e
+from scipy.constants import epsilon_0
+from scipy.constants import k
+from scipy.constants import c
+from scipy.constants import hbar
+from scipy.constants import h
+from scipy.constants import physical_constants
+
+M=physical_constants['atomic mass constant'][0]
+mb=138*M
