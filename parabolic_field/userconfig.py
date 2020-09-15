4796c1881e7f3a457310db4e536afe9535568780
Ll1ves5 <yezeyu2000@outlook.com>
2020-09-16 07:29:58 +0800

    Static parabolic field simulation

---
+++ parabolic_field/userconfig.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Zeyu Ye
+@desc Introduce the configuration of the trap and the simulation method.
+@date 9/16/2020
+"""
+
+import numpy as np
+from mdconst import *
+
+m = 138*M
+kappa = Q**2 / (4 * PI * EPSILON_0)
+
+# initial
+start_area = 20e-6
+T = 150
+
+# simulation
+twindow = 5e-3
+tstep = 5e-9
+
+# grid properties
+gmax = .005 # max and min grid point position
+size = 401 # number of grid points
+
+# fitting properties
+n_points = 21 # number of points for fitting
+grid = gmax * 2 / size * n_points
+
+# virtual gas
+m_g=m/100
+T_g=2e-3
+mu_g=np.sqrt(8* K *T_g/(PI*m_g)) # mean
+sigma_g=np.sqrt(K*T_g/(2*m_g)) # std
